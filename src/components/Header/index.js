4a98c15b7c4aa56e1a035cb73509a90d70bfccfb
dhruv-growexxer <dhruv.chauhan@growexx.com>
2022-04-18 15:46:54 +0530

    avatar added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,37 @@
-import { Header } from 'antd/lib/layout/layout'
+import { Header } from "antd/lib/layout/layout";
+import { UserOutlined } from "@ant-design/icons";
 
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
-import React from 'react'
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import React from "react";
 
-import { useSelector, useDispatch } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { toggleSide } from '../../state/actions/toggleActions'
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { toggleSide } from "../../state/actions/toggleActions";
+import { Avatar } from "antd";
 
 const MyHeader = () => {
-  const stateCollapsed = useSelector((state) => state.toggleSide)
+  const stateCollapsed = useSelector((state) => state.toggleSide);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const toggleSide1 = bindActionCreators(toggleSide, dispatch)
+  const toggleSide1 = bindActionCreators(toggleSide, dispatch);
 
   return (
-    <Header className='site-layout-background' style={{ padding: 0 }}>
+    <Header className="site-layout-background" style={{ padding: 0 }}>
       {stateCollapsed ? (
         <MenuUnfoldOutlined
           onClick={() => toggleSide1(stateCollapsed)}
-          className='trigger'
+          className="trigger"
         />
       ) : (
         <MenuFoldOutlined
           onClick={() => toggleSide1(stateCollapsed)}
-          className='trigger'
+          className="trigger"
         />
       )}
+      <Avatar size={35} icon={<UserOutlined />} className="header-avatar" />
     </Header>
-  )
-}
+  );
+};
 
-export default MyHeader
+export default MyHeader;
