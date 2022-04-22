2efd546c541b902a3d4cfaee0409a4996b8fbc5c
dhruv-growexxer <dhruv.chauhan@growexx.com>
2022-04-22 19:19:00 +0530

    Data problem

--- src/pages/Covid/District.js
+++ src/pages/Covid/District.js
@@ -6,34 +6,44 @@ const District = ({ states }) => {
     console.log(states);
   }, []);
   return (
-    <Row gutter={16}>
+    <>
       <List
-        grid={{
-          gutter: 16,
-          xs: 1,
-          sm: 2,
-          md: 4,
-          lg: 4,
-          xl: 6,
-          xxl: 3,
-        }}
+        className="wrapper"
         dataSource={states}
         renderItem={(item) => (
-          <List.Item>
-            <Card title={item.name}>Card content</Card>
-          </List.Item>
+          <Card bordered={true} title={item.name}>
+            {item.recovered}
+          </Card>
         )}
       />
-      {/* {states.map((item, index) => (
-        <Col span={6} key={index}>
-          <Card>
-            <h1>{item.name}</h1>
-            <h1>Confirmed cases - {item.confirmed}</h1>
-            <h1>Recovered cases - {item.recovered}</h1>
-          </Card>
-        </Col>
-      ))} */}
-    </Row>
+    </>
+    // <Row className="scroll">
+    //   <List
+    //     grid={{
+    //       gutter: 16,
+    //       column: 4,
+    //     }}
+    //     dataSource={states}
+    //     renderItem={(item) => (
+    //       <Col>
+    //         <List.Item>
+    //           <Card bordered={true} title={item.name}>
+    //             {item.recovered}
+    //           </Card>
+    //         </List.Item>
+    //       </Col>
+    //     )}
+    //   />
+    // {/* {states.map((item, index) => (
+    //     <Col span={6} key={index}>
+    //       <Card>
+    //         <h1>{item.name}</h1>
+    //         <h1>Confirmed cases - {item.confirmed}</h1>
+    //         <h1>Recovered cases - {item.recovered}</h1>
+    //       </Card>
+    //     </Col>
+    //   ))} */}
+    // </Row>
   );
 };
 
